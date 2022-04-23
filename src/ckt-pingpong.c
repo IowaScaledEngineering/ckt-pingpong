780a79a20c1a857717d190337bad949287758f47
Nathan D. Holmes <maverick@drgw.net>
2022-04-23 15:10:24 -0600

    Update copyright date and header version

--- src/ckt-pingpong.c
+++ src/ckt-pingpong.c
@@ -1,11 +1,11 @@
 /*************************************************************************
-Title:    CKT-PINGPONG v1.0
+Title:    CKT-PINGPONG v1.2
 Authors:  Nathan D. Holmes <maverick@drgw.net>
 File:     $Id: $
 License:  GNU General Public License v3
 
 LICENSE:
-    Copyright (C) 2020 Nathan D. Holmes (maverick@drgw.net)
+    Copyright (C) 2022 Nathan D. Holmes (maverick@drgw.net)
      & Michael Petersen (railfan@drgw.net)
 
     This program is free software; you can redistribute it and/or modify
@@ -416,7 +416,7 @@ void drawSplashScreen()
 	lcd_gotoxy(0,1);
 	lcd_puts_p(PSTR(" Shuttle Controller "));
 	lcd_gotoxy(0,2);
-	lcd_puts_p(PSTR("Iowa Scaled Eng 2020"));
+	lcd_puts_p(PSTR("Iowa Scaled Eng 2022"));
 	lcd_gotoxy(0,3);
 	lcd_puts_p(PSTR("  www.iascaled.com  "));
 	
